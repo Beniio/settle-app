6c094314deb95dea07910e7aec3d6c047205428a
Ben <benio@Bens-MacBook-Air.local>
2021-12-09 21:39:45 +0100

    Added migrations folder path to config.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,11 @@
 import {MikroORM} from "@mikro-orm/core";
 import { _prod_ } from "./constants";
 import { Post } from "./entities/Post";
+import mikroOrmConfig from "./mikro-orm.config";
 
 const main = async () => {
-    const orm = await MikroORM.init({
-        entities:[Post],
-        dbName:'settle',
-        type:'postgresql',
-        debug: !_prod_
-    });
-    
+    console.log(__dirname);
+    const orm = await MikroORM.init(mikroOrmConfig);
     const post = orm.em.create(Post,{title: "example"});
     await orm.em.persistAndFlush(post);
 }
--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -1,9 +1,15 @@
+import { MikroORM } from "@mikro-orm/core";
+import path from "path/posix";
 import { _prod_ } from "./constants";
 import { Post } from "./entities/Post";
 
 export default {
+    migrations:{
+        path: path.join(__dirname,"./migrations"),
+        pattern: /^[\w-]+\d+\.[tj]s$/,
+    },
     entities: [Post],
     dbName: 'settle',
     type: 'postgresql',
     debug: !_prod_
-} as const;
+} as Parameters<typeof MikroORM.init>[0];
