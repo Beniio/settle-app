775ff148f66c40bc6cd0b0cfd31f3af19836059b
Ben <benio@Bens-MacBook-Air.local>
2021-12-10 22:42:49 +0100

    Added graphql.

---
+++ server/dist/constants.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports._prod_ = void 0;
+exports._prod_ = process.env.NODE_ENV === 'production';
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,31 @@
 import {MikroORM} from "@mikro-orm/core";
 import { _prod_ } from "./constants";
-import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+import express from 'express';
+import {ApolloServer} from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
-    console.log(__dirname);
     const orm = await MikroORM.init(mikroOrmConfig);
-    const post = orm.em.create(Post,{title: "example"});
-    await orm.em.persistAndFlush(post);
+    await orm.getMigrator().up();
+
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+          resolvers: [HelloResolver],
+          validate: false,
+      }),
+    });
+
+    apolloServer.applyMiddleware({app})
+
+    app.listen(4000, () => {
+        console.log('server started on localhost:4000')
+    })
 }
 
-main();
+main().catch((err)=>{
+    console.log(err)
+});
