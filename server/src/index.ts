23cf6c31868d2b0a18474b203c2ec9eb02322f61
Ben <beniamin.gadecki@gmail.com>
2021-12-12 17:36:51 +0100

    Added Mutation in resolver's post.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import "reflect-metadata";
+import "reflect-metadata"; 
 import {MikroORM} from "@mikro-orm/core";
 import { _prod_ } from "./constants";
 import mikroOrmConfig from "./mikro-orm.config";
