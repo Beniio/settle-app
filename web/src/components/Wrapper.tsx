6113b56b0a8f8df19cf88fcb56e2b1db3fb22962
Ben <beniamin.gadecki@gmail.com>
2021-12-30 21:08:44 +0100

    Added wrapper and FormLabel

---
+++ web/src/components/Wrapper.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/core';
+import React from 'react';
+
+interface WrapperProps {}
+
+export const Wrapper: React.FC<WrapperProps> = ({}) => {
+  return (
+      <Box>
+          
+      </Box>
+  );  
+}
--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import {Form, Formik} from 'formik';
+import { FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/core';
+import { Wrapper } from '../components/Wrapper';
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
     return (
-      <Formik>
-          {() =>(
+        <Wrapper>
+          <Formik 
+        initialValues={{username: "", password: "" }}
+        onSubmit={(values) => {
+            console.log(values)
+        }}>
+          {({values, handleChange}) =>(
               <Form>
-                  <></>
+                  <FormControl>
+                      <FormLabel htmlFor="username">First name</FormLabel>
+                      <Input 
+                        value={values.username} 
+                        onChange={handleChange}
+                        id="name"
+                        placeholder="name" />
+                      {/* <FormErrorMessage>{form.error.name}</FormErrorMessage> */}
+                  </FormControl>
               </Form>
           ) }
       </Formik>
+        </Wrapper>
     )
-
 }
