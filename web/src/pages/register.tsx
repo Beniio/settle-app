eaabc2479e72eaec4878ae9a3048fbee2407d211
Ben <beniamin.gadecki@gmail.com>
2022-01-02 20:50:04 +0100

    Added register form

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,32 +1,38 @@
 import React from 'react';
 import {Form, Formik} from 'formik';
-import { FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/core';
+import { Box, Button, FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/core';
 import { Wrapper } from '../components/Wrapper';
+import { InputField } from '../components/InputField';
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
     return (
-        <Wrapper>
+        <Wrapper variant="small">
         <Formik 
-        initialValues={{username: "", password: "" }}
-        onSubmit={(values) => {
-            console.log(values)
-        }}>
-          {({values, handleChange}) =>(
-              <Form>
-                  <FormControl>
-                      <FormLabel htmlFor="username">First name</FormLabel>
-                      <Input 
-                        value={values.username} 
-                        onChange={handleChange}
-                        id="name"
-                        placeholder="name" />
-                      {/* <FormErrorMessage>{form.error.name}</FormErrorMessage> */}
-                  </FormControl>
-              </Form>
-          ) }
-      </Formik>
+            initialValues={{username: "", password: "" }}
+            onSubmit={(values) => {
+                console.log(values)
+            }}>
+            {({values, handleChange}) =>(
+                <Form>
+                   <InputField 
+                    name='username'
+                    placeholder="username"
+                    label="Username"
+                    />
+                    <Box mt={4}>
+                     <InputField 
+                        name="password"
+                        placeholder="password"
+                        label="Password"
+                        type="password"
+                      />
+                    </Box>
+                    <Button type='submit' variantColor='teal'>register</Button>
+                </Form>
+            ) }
+        </Formik>
         </Wrapper>
     )
 }
