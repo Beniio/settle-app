03fc5fbeb77ec0d9574cbf289e2dc0349e9f4779
Ben <beniamin.gadecki@gmail.com>
2022-01-08 19:49:52 +0100

    Configured prettier

--- web/src/components/Wrapper.tsx
+++ web/src/components/Wrapper.tsx
@@ -2,13 +2,21 @@ import { Box } from '@chakra-ui/core';
 import React from 'react';
 
 interface WrapperProps {
-    variant?: 'small' | 'regular'
+  variant?: 'small' | 'regular';
 }
 
-export const Wrapper: React.FC<WrapperProps> = ({children,variant= "regular"}) => {
+export const Wrapper: React.FC<WrapperProps> = ({
+  children,
+  variant = 'regular'
+}) => {
   return (
-		<Box mt={8} mx= "auto" maxW={variant==="regular" ? "800px": "400px"} w="100%">
-			{children}
-		</Box>
-  );  
-}
+    <Box
+      mt={8}
+      mx="auto"
+      maxW={variant === 'regular' ? '800px' : '400px'}
+      w="100%"
+    >
+      {children}
+    </Box>
+  );
+};
--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Form, Formik} from 'formik';
+import { Form, Formik } from 'formik';
 import { Box, Button } from '@chakra-ui/core';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
@@ -7,32 +7,35 @@ import { InputField } from '../components/InputField';
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
-	return (
-		<Wrapper variant="small">
-			<Formik 
-				initialValues={{username: "", password: "" }}
-				onSubmit={(values) => {
-						console.log(values)
-				}}>
-				{({values, handleChange}) =>(
-					<Form>
-							<InputField 
-							name='username'
-							placeholder="username"
-							label="Username"
-							/>
-							<Box mt={4}>
-								<InputField 
-									name="password"
-									placeholder="password"
-									label="Password"
-									type="password"
-								/>
-							</Box>
-							<Button type='submit' variantColor='teal'>register</Button>
-					</Form>
-				)}
-			</Formik>
-			</Wrapper>
-    )
-}
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ values, handleChange }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button type="submit" variantColor="teal">
+              register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
