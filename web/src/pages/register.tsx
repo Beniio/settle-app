63b2af38f3e5115f62abf560fa33a79d5c096d08
Ben <beniamin.gadecki@gmail.com>
2022-01-19 21:35:58 +0100

    Changed folder structure

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -3,23 +3,24 @@ import { Form, Formik } from 'formik';
 import { Box, Button } from '@chakra-ui/core';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
-import { useMutation } from 'urql';
+import { useRegisterMutation } from '../generated/graphql';
 
 interface registerProps {}
 
-const REGISTER_MUT = `
-
-`;
-
 const Register: React.FC<registerProps> = ({}) => {
-  const [, register] = useMutation(REGISTER_MUT);
+  const [, register] = useRegisterMutation();
 
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: '', password: '' }}
-        onSubmit={async (values) => {
+        onSubmit={async (values, { setErrors }) => {
           const response = await register(values);
+          if (response.data?.register) {
+            setErrors({
+              username: 'error'
+            });
+          }
         }}
       >
         {({ isSubmitting }) => (
