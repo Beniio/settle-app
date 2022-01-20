d684fe4bd3900ef4723f275fa1e1be4fe6e53ef9
Ben <beniamin.gadecki@gmail.com>
2022-01-20 22:30:02 +0100

    Added utis folder and errorMap

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -4,6 +4,7 @@ import { Box, Button } from '@chakra-ui/core';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
 import { useRegisterMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
 
 interface registerProps {}
 
@@ -16,10 +17,11 @@ const Register: React.FC<registerProps> = ({}) => {
         initialValues={{ username: '', password: '' }}
         onSubmit={async (values, { setErrors }) => {
           const response = await register(values);
-          if (response.data?.register) {
-            setErrors({
-              username: 'error'
-            });
+          if (response.data?.register?.errors) {
+            setErrors(
+              toErrorMap(response.data.register.errors)
+            );
+          } else {
           }
         }}
       >
