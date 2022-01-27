eb01fbb5fec2fde1a0ed2328ebb3888ee62c4265
Ben <beniamin.gadecki@gmail.com>
2022-01-27 22:01:49 +0100

    Implementing logout

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,18 +1,50 @@
 import { Box, Link } from '@chakra-ui/layout';
 import React from 'react';
 import NextLink from 'next/link';
+import { useMeQuery } from '../generated/graphql';
+import { Button, Flex } from '@chakra-ui/core';
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
-  return (
-    <Box bg="turquoise" p={4} ml={'auto'}>
-      <NextLink href="/login">
-        <Link mr={2}> Login </Link>
-      </NextLink>
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
 
-      <NextLink href="/register">
-        <Link mr={2}> Register </Link>
-      </NextLink>
-    </Box>
+  if (fetching) {
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2}> login </Link>
+        </NextLink>
+
+        <NextLink href="/register">
+          <Link mr={2}> register </Link>
+        </NextLink>
+      </>
+    );
+  } else {
+    body = (
+      <Flex align="center">
+        <Box mr={4}>{data.me.username}</Box>
+        <Button variant="link">logout</Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex
+      zIndex={1}
+      position="sticky"
+      top={0}
+      bg="tan"
+      p={4}
+    >
+      <Flex flex={1} m="auto" align="center" maxW={800}>
+        <NextLink href="/">
+          <Link>SettleApp</Link>
+        </NextLink>
+        <Box ml={'auto'}>{body}</Box>
+      </Flex>
+    </Flex>
   );
 };
--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,12 +1,80 @@
 import { CSSReset, ThemeProvider } from '@chakra-ui/core';
-import { Provider, createClient } from 'urql';
+import {
+  QueryInput,
+  Cache,
+  cacheExchange
+} from '@urql/exchange-graphcache';
+import { parseConfigFileTextToJson } from 'typescript';
+import {
+  Provider,
+  createClient,
+  dedupExchange,
+  fetchExchange
+} from 'urql';
+import {
+  LoginMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation
+} from '../generated/graphql';
 import theme from '../theme';
+
+const betterUpdateQuery = <Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) => {
+  return cache.updateQuery(
+    qi,
+    (data) =>
+      parseConfigFileTextToJson(result, data as any) as any
+  );
+};
 
 const client = createClient({
   url: 'http://localhost:4000/graphql',
   fetchOptions: {
     credentials: 'include'
-  }
+  },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return { me: result.login.user };
+                }
+              }
+            );
+          },
+          register: (_result, args, cache, info) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return { me: result.register.user };
+                }
+              }
+            );
+          }
+        }
+      }
+    }),
+    fetchExchange
+  ]
 });
 
 function MyApp({ Component, pageProps }: any) {
