11f968d8f2576b59394955a69ca0a6cf603e2bb9
Beniamin Gadecki <beniamin.gadecki@gmail.com>
2022-04-04 22:18:48 +0200

    refactor

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { NavBar } from '../components/NavBar';
 import { withUrqlClient } from 'next-urql';
 import { createUrqlClient } from '../utils/createUrqlClient';
-import { usePostsQuery } from '../generated/graphql';
 
 const Index = () => {
   return (
