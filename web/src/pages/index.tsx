6cefc89693f305d4b1b63e071b7ac4cb7e358564
Beniamin Gadecki <beniamin.gadecki@gmail.com>
2022-04-04 22:18:35 +0200

    refactor

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -4,17 +4,9 @@ import { createUrqlClient } from '../utils/createUrqlClient';
 import { usePostsQuery } from '../generated/graphql';
 
 const Index = () => {
-  const [{ data }] = usePostsQuery();
   return (
     <>
       <NavBar />
-      {!data ? (
-        <div>Loading...</div>
-      ) : (
-        data.posts.map((p) => (
-          <div key={p.id}>{p.title}</div>
-        ))
-      )}
     </>
   );
 };
